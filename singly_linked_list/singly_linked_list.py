222859ecb0cf1d8eefdc6d6702db65f0112fe705
jwross24 <jwross24@users.noreply.github.com>
2020-05-11 13:15:24 -0400

    add linked list traversal

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,7 +1,10 @@
 class LinkedList:
     def __init__(self, nodes=None):
+        '''
+        Initialize the linked list, with optional input data.
+        '''
         self.head = None
-        if nodes is not None:
+        if nodes:
             node = Node(value=nodes.pop(0))
             self.head = node
             for elem in nodes:
@@ -9,34 +12,45 @@ class LinkedList:
                 node = node.next
 
     def __str__(self):
+        '''
+        String representation of the linked list.
+        '''
         node = self.head
         nodes = []
-        while node is not None:
+        while node:
             nodes.append(node.value)
             node = node.next
         nodes.append("None")
         return " -> ".join(nodes)
 
+    def __iter__(self):
+        '''
+        Pythonic traversal through a linked list.
+        '''
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+
 
 class Node:
     def __init__(self, value):
+        '''
+        Initialize a node with a value.
+        '''
         self.value = value
         self.next = None
 
     def __str__(self):
+        '''
+        String representation of a node.
+        '''
         return self.value
 
 
 if __name__ == '__main__':
-    llist = LinkedList()
+    llist = LinkedList(['a', 'b', 'c', 'd', 'e'])
     print(llist)
 
-    first_node = Node('a')
-    llist.head = first_node
-    print(llist)
-
-    second_node = Node('b')
-    third_node = Node('c')
-    first_node.next = second_node
-    second_node.next = third_node
-    print(llist)
+    for node in llist:
+        print(node)
