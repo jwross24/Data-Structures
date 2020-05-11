557cb2bf14e988c002e63653b6d9493d4003ca61
jwross24 <jwross24@users.noreply.github.com>
2020-05-11 16:15:57 -0400

    revert queue to two stacks

--- queue/queue.py
+++ queue/queue.py
@@ -1,10 +1,9 @@
-from singly_linked_list.singly_linked_list import LinkedList
-# from stack.stack import Stack
+# from singly_linked_list.singly_linked_list import LinkedList
+from stack.stack import Stack
 
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order.
-
 1. Implement the Queue class using an array as the underlying storage
    structure. Make sure the Queue tests pass.
 2. Re-implement the Queue class, this time using the linked list implementation
@@ -12,7 +11,6 @@ return elements in First In First Out order.
    Make sure the Queue tests pass.
 3. What is the difference between using an array vs. a linked list when
    implementing a Queue?
-
 Stretch: What if you could only use instances of your Stack class to implement
          the Queue? What would that look like? How many Stacks would you need?
          Try it!
@@ -21,18 +19,44 @@ Stretch: What if you could only use instances of your Stack class to implement
 
 class Queue:
     def __init__(self):
-        self.size = 0
-        self.storage = LinkedList()
+        self.stack1 = Stack()
+        self.stack2 = Stack()
 
     def __len__(self):
-        return self.size
+        return len(self.stack1) + len(self.stack2)
 
     def enqueue(self, value):
-        self.storage.add_to_tail(value)
-        self.size += 1
+        self.stack1.push(value)
 
     def dequeue(self):
-        if len(self) < 1:
-            return None
-        self.size -= 1
-        return self.storage.remove_head()
+        if self.stack2.size < 1:
+            if self.stack1.size < 1:  # if both stacks are empty
+                return None
+            else:
+                # Move elements from stack 1 to stack 2 only if stack 2 is
+                # empty
+                while len(self.stack1) >= 1:
+                    x = self.stack1.pop()
+                    self.stack2.push(x)
+
+        return self.stack2.pop()
+
+
+if __name__ == '__main__':
+    q = Queue()
+    q.enqueue(100)
+
+    print(len(q.stack1))
+    q.enqueue(101)
+
+    print(len(q.stack1))
+    q.enqueue(105)
+
+    print(len(q.stack1))
+
+    print(q.stack1.storage)
+    print(q.stack2.storage)
+
+    print(q.dequeue())
+    print(q.stack2.storage)
+    print(len(q.stack2))
